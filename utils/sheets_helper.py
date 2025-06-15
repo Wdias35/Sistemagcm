71f75e4dbc9a78728ec7569003ca04d7499a6643
Wdias35 <wagner-martins25@hotmail.com>
2025-06-15 19:11:38 -0300

    Update sheets_helper.py

--- utils/sheets_helper.py
+++ utils/sheets_helper.py
@@ -1,36 +1,26 @@
-from google.oauth2.service_account import Credentials
-import gspread
 import streamlit as st
+import gspread
+from google.oauth2.service_account import Credentials
 
 class SheetsHelper:
     def __init__(self):
-        # Pega o dict do secrets
         creds_dict = st.secrets["creds"]
-        scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-        creds = Credentials.from_service_account_info(st.secrets["creds"], scopes=scopes)
+        credentials = Credentials.from_service_account_info(creds_dict)
+        client = gspread.authorize(credentials)
+        self.sheet = client.open_by_key("1K8YAEd9bSBS4W-fMgWQ_uMPfHJF22UFpSe-Ihv5CSnM").sheet1
 
-        client = gspread.authorize(creds)
-
-        # Pega o ID da planilha também dos secrets
-        self.sheet = client.open_by_key(st.secrets["sheet_id"]).sheet1
-
-    def inserir_ocorrencia(self, dados):
+    def inserir_ocorrencia(self, registro):
         try:
-            self.sheet.append_row(list(dados.values()))
+            self.sheet.append_row(list(registro.values()))
             return True
-        except Exception as e:
-            st.error(f"Erro ao inserir ocorrência: {e}")
+        except:
             return False
 
     def ler_todas_ocorrencias(self, mestre=False):
         try:
-            dados = self.sheet.get_all_records()
+            data = self.sheet.get_all_records()
             if mestre:
-                return dados
-            else:
-                # Filtro por base
-                base = st.session_state["login"]
-                return [d for d in dados if d["Base Responsável"] == base]
-        except Exception as e:
-            st.error(f"Erro ao ler ocorrências: {e}")
+                return data
+            return data  # Filtragem por base se quiser
+        except:
             return []

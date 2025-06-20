c05b67879116190ff146418abebf8a9012b5bb31
Wdias35 <wagner-martins25@hotmail.com>
2025-06-20 11:04:18 +0000

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def main():
             local = st.text_input("Local")
             tipo = st.selectbox("Tipo", ["Abordagem", "Veículo Recolhido", "Crime", "Prisão em Flagrante", "Procurado Capturado"])
             observacoes = st.text_area("Observações")
-              latitude = st.text_input("Latitude (ex: -23.4543)")
+            latitude = st.text_input("Latitude (ex: -23.4543)")
             longitude = st.text_input("Longitude (ex: -46.5333)")
 
             enviar = st.form_submit_button("Enviar")
